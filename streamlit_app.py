ab98f465f30a9b81197f029eb7a558779609b7ff
anapm21 <152014376+anapm21@users.noreply.github.com>
2023-11-25 23:18:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.markdown("<h1 style='text-align: center; color: white;'>Mejora tu propiedad c
 st.markdown("<h2 style='text-align: center; color: white;'>Carga aqui tu imagen</h2>", unsafe_allow_html=True)
 
 imagefelecha = Image.open('felecha.png')
-st.image(imagefelecha, caption='',aling:center)
+st.image(imagefelecha, caption='',aling: center)
 
 #st.markdown("App mejora de propiedades")
 #st.button('Click')
