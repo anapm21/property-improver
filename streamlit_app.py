b9d9ffd0e0f378846866367549d716c9e1e64060
anapm21 <152014376+anapm21@users.noreply.github.com>
2023-11-25 23:01:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ from PIL import Image
 
 #st.title("property-improver")
 
-st.markdown("<h1 style='text-align: center; color: white;'>Mejora tu propiedad con Property Improve</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: white;'>Mejora tu propiedad con </h2>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>Property Improve</h1>", unsafe_allow_html=True)
 
 st.markdown("<h2 style='text-align: center; color: white;'>Carga aqui tu imagen</h2>", unsafe_allow_html=True)
 
