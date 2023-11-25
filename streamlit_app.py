62d7e98515acebad2828ce53e7c126a0c6251097
anapm21 <152014376+anapm21@users.noreply.github.com>
2023-11-25 22:56:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,12 @@ from PIL import Image
 
 st.title("property-improver")
 
-st.markdown("<h1 style='text-align: center; color: white;'>Big headline</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>Mejora tu propiedad con Property Improve</h1>", unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center; color: black;'>Smaller headline in black </h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: white;'>Carga tu imagen</h2>", unsafe_allow_html=True)
 
-st.markdown("App mejora de propiedades")
-st.button('Click')
+#st.markdown("App mejora de propiedades")
+#st.button('Click')
 image = Image.open('img_1.jpg')
 
 st.image(image, caption='Sunrise by the mountains')
