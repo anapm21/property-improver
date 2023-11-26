ae1ec519c11f1d9d28a0c47f2e5e4f13b4a0fed9
anapm21 <152014376+anapm21@users.noreply.github.com>
2023-11-26 03:20:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.markdown("<h2 style='text-align: center; color: white;'>Carga aqui tu imagen<
 col1, col2 = st.columns (2)
 
 with col1:
- st.image(house1.jpg)
+ st.image(img_1.jpg)
  st.button ("Casa 1")
  st.image(house3.jpg)
  st.button ("Casa 2")
